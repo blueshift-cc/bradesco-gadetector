8c625190040bd166e597e03578866cc4f9ab41e9
Willerson Stockler <willersonsp@hotmail.com>
2024-05-20 09:12:07 -0300

    Melhorado o tratamento de erros.

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -3,7 +3,8 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import fetch from "node-fetch";
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:81', 'https://gadetector.bohr.io', 'https://gadetector.blueshift.cc'];
+//const allowedOrigins = ['http://localhost:3000', 'http://localhost:80', 'https://gadetector.bohr.io', 'https://gadetector.blueshift.cc'];
+const allowedOrigins = ['http://localhost:3000', 'http://localhost', 'http://localhost:81', 'https://gadetector.bohr.io', 'https://gadetector.blueshift.cc'];
 
 const options: cors.CorsOptions = {
   origin: allowedOrigins
@@ -13,6 +14,18 @@ var app = express();
 app.use(cors(options));
 app.use(bodyParser.json({ limit: "100mb" }))
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
+
+function isValidHttpUrl(string: any) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
 
 async function isGA3UA(data: any) {
   const regexTag = /(['"]UA-[a-zA-Z0-9-]*['"])/m;
@@ -55,36 +68,51 @@ app.get("/", function (req: Request, res: Response) {
 });
 
 app.post("/process", async function (req: Request, res: Response) {
-  // console.log(req);
-  // res.send("ok");
-  // return;
-  //const urls: string[] = req.body?.urlTextArea.split(";");
-  const urls: string[] = req.body?.urlTextArea.split(/[;\n ]/);
 
-  const urlsDeDuplicated = [...new Set(urls)];
+  var responseData: any = [];
 
-  var responseData = [];
-  for (let i = 0; i < urlsDeDuplicated.length; i++) {
-    const servers = await fetch(urlsDeDuplicated[i]);
-    const data = await servers.text();
-    const is_ga3 = await isGA3UA(data);
-    const is_ga3gtm = await isGA3GTM(data);
-    const is_ga4 = await isGA4(data);
+  try {
+    const urTextArea: String = req.body?.urlTextArea;
 
-    if (is_ga3 != null) {
-      responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3.slice(1, -1) });
+    const urls: string[] = urTextArea.split(/[;\n ]/);
+
+    const urlsDeDuplicated = [...new Set(urls)];
+
+    for (let i = 0; i < urlsDeDuplicated.length; i++) {
+
+      if (!isValidHttpUrl(urlsDeDuplicated[i])) {
+        continue;
+      }
+      await fetch(urlsDeDuplicated[i]).then(async (response: any) => {
+        const data = await response.text();
+        const is_ga3 = await isGA3UA(data);
+        const is_ga3gtm = await isGA3GTM(data);
+        const is_ga4 = await isGA4(data);
+
+        if (is_ga3 != null) {
+          responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3.slice(1, -1) });
+        }
+        if (is_ga3gtm != null) {
+          responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3gtm.slice(1, -1) });
+        }
+        if (is_ga4 != null) {
+          responseData.push({ "url": urlsDeDuplicated[i], "version": 4, tag: is_ga4.slice(1, -1) });
+        }
+        if (is_ga3 == null && is_ga3gtm == null && is_ga4 == null) {
+          responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'sem_tag' });
+        }
+      }).catch((e: any) => {
+        console.log(e);
+        responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'offline' });
+      });
     }
-    if (is_ga3gtm != null) {
-      responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3gtm.slice(1, -1) });
-    }
-    if (is_ga4 != null) {
-      responseData.push({ "url": urlsDeDuplicated[i], "version": 4, tag: is_ga4.slice(1, -1) });
-    }
-    if (is_ga3 == null && is_ga3gtm == null && is_ga4 == null) {
-      responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'sem_tag' });
-    }
+    res.json(responseData);
   }
-  res.json(responseData);
+  catch (e) {
+    res.status(500);
+    res.send(JSON.stringify(e));
+    return;
+  }
 });
 
 if (!module.parent) {
