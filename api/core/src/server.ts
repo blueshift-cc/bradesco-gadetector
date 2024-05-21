728418c1ca673b4e139f7d8bfb388317a4d0531f
Willerson Stockler <willersonsp@hotmail.com>
2024-05-21 16:38:02 -0300

    remove duplicate.

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -130,13 +130,13 @@ app.post("/process", async function (req: Request, res: Response) {
           responseData.push({ "url": urlsDeDuplicated[i], "version": tag_ver, tag: tags_.toString(), globalJS: is_globaljs, globalBI: is_globalBI });
         }
         else {
-          if (is_ga3 != null) {
+          if (is_globaljs == null && is_ga3 != null) {
             responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3.slice(1, -1), globalJS: is_globaljs, globalBI: is_globalBI });
           }
-          if (is_ga3gtm != null) {
+          if (is_globaljs == null && is_ga3gtm != null) {
             responseData.push({ "url": urlsDeDuplicated[i], "version": 4, tag: is_ga3gtm.slice(1, -1), globalJS: is_globaljs, globalBI: is_globalBI });
           }
-          if (is_ga4 != null) {
+          if (is_globaljs == null && is_ga4 != null) {
             responseData.push({ "url": urlsDeDuplicated[i], "version": 4, tag: is_ga4.slice(1, -1), globalJS: is_globaljs, globalBI: is_globalBI });
           }
           if (is_globaljs != null) {
