995a9c16baf4e7f19d6f4ae14925075526fc1944
Willerson Stockler <willersonsp@hotmail.com>
2024-05-21 15:33:45 -0300

    change logic.

--- api/core/src/server.ts
+++ api/core/src/server.ts
@@ -63,6 +63,18 @@ async function isGA4(data: any) {
   return null;
 }
 
+async function isGlobalJS(data: any) {
+  const regexTag = /GlobalJS.js/gm;
+  let m;
+
+  const tag = data.match(regexTag);
+
+  if (tag?.length > 0) {
+    return tag[0];
+  }
+  return null;
+}
+
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello world");
 });
@@ -88,32 +100,31 @@ app.post("/process", async function (req: Request, res: Response) {
         const is_ga3 = await isGA3UA(data);
         const is_ga3gtm = await isGA3GTM(data);
         const is_ga4 = await isGA4(data);
+        const is_globaljs = await isGlobalJS(data);
 
-        if (is_ga3 != null && is_ga4 != null) {
-          responseData.push({ "url": urlsDeDuplicated[i], "version": "3, 4", tag: [...new Set([is_ga3?.slice(1, -1), is_ga4?.slice(1, -1)].filter(n => n))].toString() });
-        }
-        else if (is_ga3gtm != null && is_ga4 != null) {
-          const tags_ = [...new Set([is_ga3gtm?.slice(1, -1), is_ga4?.slice(1, -1)].filter(n => n))];
-          responseData.push({ "url": urlsDeDuplicated[i], "version": (tags_.length > 1 ? "4, 4" : "4"), tag: [...new Set([is_ga3gtm?.slice(1, -1), is_ga4?.slice(1, -1)].filter(n => n))].toString() });
+        if ((is_ga3 != null || is_ga3gtm != null) && is_ga4 != null) {
+          const tags_ = [...new Set([is_ga3?.slice(1, -1), is_ga3gtm?.slice(1, -1), is_ga4?.slice(1, -1)].filter(n => n))];
+          const tag_ver = tags_.toString().indexOf('UA-') > -1 && tags_.length > 1 ? "3, 4" : tags_.length > 1 ? "4, 4" : "4";
+          responseData.push({ "url": urlsDeDuplicated[i], "version": tag_ver, tag: tags_.toString(), globalJS: is_globaljs });
         }
         else {
           if (is_ga3 != null) {
-            responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3.slice(1, -1) });
+            responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3.slice(1, -1), globalJS: is_globaljs });
           }
           if (is_ga3gtm != null) {
-            responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3gtm.slice(1, -1) });
+            responseData.push({ "url": urlsDeDuplicated[i], "version": 3, tag: is_ga3gtm.slice(1, -1), globalJS: is_globaljs });
           }
           if (is_ga4 != null) {
-            responseData.push({ "url": urlsDeDuplicated[i], "version": 4, tag: is_ga4.slice(1, -1) });
+            responseData.push({ "url": urlsDeDuplicated[i], "version": 4, tag: is_ga4.slice(1, -1), globalJS: is_globaljs });
           }
         }
 
         if (is_ga3 == null && is_ga3gtm == null && is_ga4 == null) {
-          responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'sem_tag' });
+          responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'sem_tag', globalJS: is_globaljs });
         }
       }).catch((e: any) => {
         console.log(e);
-        responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'offline' });
+        responseData.push({ "url": urlsDeDuplicated[i], "version": 0, tag: 'offline', globalJS: 'offline' });
       });
     }
     res.json(responseData);
